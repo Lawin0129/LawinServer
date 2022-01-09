0951502abad95e2d5b305fc0d77b7cc768357b74
PRO100KatYT <dominator972@wp.pl>
2022-01-09 11:23:57 +0100

    Added Winterfest 2021 Battle Royale subgame image.

--- index.js
+++ index.js
@@ -5981,6 +5981,7 @@ function getContentPages(req) {
         if (seasondata.build == 19.01) {
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].stage = "winter2021";
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/t-bp19-lobby-xmas-2048x1024-f85d2684b4af.png";
+            contentpages.subgameinfo.battleroyale.image = "https://cdn2.unrealengine.com/19br-wf-subgame-select-512x1024-16d8bb0f218f.jpg";
             contentpages.specialoffervideo.bSpecialOfferEnabled = "true";
         }
     } catch (err) {}
