7a97ca74694e6777307898fba78e1faeb8b6934b
PRO100KatYT <67335438+PRO100KatYT@users.noreply.github.com>
2022-12-03 09:07:10 +0100

    Fixed not being able to log in on latest (EOS).

--- structure/user.js
+++ structure/user.js
@@ -65,6 +65,52 @@ express.get("/account/api/public/account/:accountId", async (req, res) => {
     })
 })
 
+express.get("/sdk/v1/*", async (req, res) => {
+    const sdk = require("./../responses/sdkv1.json");
+    res.json(sdk)
+})
+
+express.post("/auth/v1/oauth/token", async (req, res) => {
+    res.json({
+        "access_token": "lawinstokenlol",
+        "token_type": "bearer",
+        "expires_in": 28800,
+        "expires_at": "9999-12-31T23:59:59.999Z",
+        "deployment_id": "lawinsdeploymentidlol",
+        "organization_id": "lawinsorganizationidlol",
+        "product_id": "prod-fn",
+        "sandbox_id": "fn"
+    })
+})
+
+express.get("/epic/id/v2/sdk/accounts", async (req, res) => {
+    res.json([{
+        "accountId": Memory_CurrentAccountID,
+        "displayName": Memory_CurrentAccountID,
+        "preferredLanguage": "en",
+        "cabinedMode": false,
+        "empty": false
+    }])
+})
+
+express.post("/epic/oauth/v2/token", async (req, res) => {
+    res.json({
+        "scope": "basic_profile friends_list openid presence",
+        "token_type": "bearer",
+        "access_token": "lawinstokenlol",
+        "expires_in": 28800,
+        "expires_at": "9999-12-31T23:59:59.999Z",
+        "refresh_token": "lawinstokenlol",
+        "refresh_expires_in": 86400,
+        "refresh_expires_at": "9999-12-31T23:59:59.999Z",
+        "account_id": Memory_CurrentAccountID,
+        "client_id": "lawinsclientidlol",
+        "application_id": "lawinsapplicationidlol",
+        "selected_account_id": Memory_CurrentAccountID,
+        "id_token": "lawinstokenlol"
+    })
+})
+
 express.get("/account/api/public/account/*/externalAuths", async (req, res) => {
     res.json([])
 })
