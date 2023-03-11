628ddb6133b4757635676031696c8b82c16d9fd7
PRO100KatYT <67335438+PRO100KatYT@users.noreply.github.com>
2023-03-11 08:14:38 +0100

    Added the S24 lobby background.

--- structure/functions.js
+++ structure/functions.js
@@ -241,45 +241,44 @@ function getContentPages(req) {
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].stage = "seasonx";
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[1].stage = "seasonx";
         }
-
-        if (memory.build == 11.31 || memory.build == 11.40) {
+        else if (memory.build == 11.31 || memory.build == 11.40) {
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].stage = "Winter19";
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[1].stage = "Winter19";
         }
-
-        if (memory.build == 19.01) {
+        else if (memory.build == 19.01) {
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].stage = "winter2021";
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/t-bp19-lobby-xmas-2048x1024-f85d2684b4af.png";
             contentpages.subgameinfo.battleroyale.image = "https://cdn2.unrealengine.com/19br-wf-subgame-select-512x1024-16d8bb0f218f.jpg";
             contentpages.specialoffervideo.bSpecialOfferEnabled = "true";
         }
-
-        if (memory.season == 20) {
+        else if (memory.season == 20) {
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/t-bp20-lobby-2048x1024-d89eb522746c.png";
             if (memory.build == 20.40) {
                 contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/t-bp20-40-armadillo-glowup-lobby-2048x2048-2048x2048-3b83b887cc7f.jpg"
             }
         }
-
-        if (memory.season == 21) {
+        else if (memory.season == 21) {
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/s21-lobby-background-2048x1024-2e7112b25dc3.jpg"
             if (memory.build == 21.30) {
                 contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/nss-lobbybackground-2048x1024-f74a14565061.jpg";
                 contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].stage = "season2130";
             }
         }
-
-        if (memory.season == 22) {
+        else if (memory.season == 22) {
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/t-bp22-lobby-square-2048x2048-2048x2048-e4e90c6e8018.jpg"
         }
 
-        if (memory.season == 23) {
+        else if (memory.season == 23) {
             contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/t-bp23-lobby-2048x1024-2048x1024-26f2c1b27f63.png"
             if (memory.build == 23.10) {
                 contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/t-bp23-winterfest-lobby-square-2048x2048-2048x2048-277a476e5ca6.png"
                 contentpages.specialoffervideo.bSpecialOfferEnabled = "true";
             }
         }
+        else if (memory.season == 24) {
+            contentpages.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/t-ch4s2-bp-lobby-4096x2048-edde08d15f7e.jpg"
+        }
+
     } catch (err) {}
 
     return contentpages;
