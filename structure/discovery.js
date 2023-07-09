fb7d00b23d0810385273824458087b1a309d2444
PRO100KatYT <67335438+PRO100KatYT@users.noreply.github.com>
2023-07-09 18:15:24 +0200

    Fix deactivated play button for v24.30+

--- structure/discovery.js
+++ structure/discovery.js
@@ -16,6 +16,24 @@ express.post("/links/api/fn/mnemonic", async (req, res) => {
     res.json(MnemonicArray);
 })
 
+express.get("/links/api/fn/mnemonic/:playlist/related", async (req, res) => {
+    var response = {
+        "parentLinks": [],
+        "links": {}
+    };
+
+    if (req.params.playlist) {
+        for (var i in discovery.Panels[0].Pages[0].results) {
+            var linkData = discovery.Panels[0].Pages[0].results[i].linkData;
+            if (linkData.mnemonic == req.params.playlist) {
+                response.links[req.params.playlist] = linkData;
+            }
+        }        
+    }    
+
+    res.json(response);
+})
+
 express.get("/links/api/fn/mnemonic/*", async (req, res) => {
     for (var i in discovery.Panels[0].Pages[0].results) {
         if (discovery.Panels[0].Pages[0].results[i].linkData.mnemonic == req.url.split("/").slice(-1)[0]) {
